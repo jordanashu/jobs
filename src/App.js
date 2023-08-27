242185e5ad01b5625773731999fc8c9fc16a36ee
Ashutosh Kumar Singh <jordanashu99@gmail.com>
2023-08-27 14:49:42 +0530

    jobs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,266 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">JOBS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Company</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>  
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Jobs</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Tcs</a></li>
+            <li><a class="dropdown-item" href="#">Wipro</a></li>
+            <li><a class="dropdown-item" href="#">Hcl</a></li>
+          </ul>
+
+         
+        </li>
+      </ul>
+      <form class="d-flex">
+      <input class="form-control me-2" type="text" placeholder="Search" />
+        <button class="btn btn-primary" type="button">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+       <div class="container-fluid  d-flex ">
+
+
+       <div class="d-flex justify-content-start w-25 flex-column mt-5">
+        <div class="border  ">
+
+          <a href="#">
+          <div class=" text-black">
+          <h4 class="float-start">Hcl</h4>
+          <br/>
+          <br/>
+            <h3 class="float-start">Fronted Developer</h3>
+            <br/> <br/>
+          
+            <h4 class="float-start">Noida</h4>
+            <br/>
+          <br/>
+            <h5 class="float-start">₹4-5 lpa</h5>
+
+ 
+  
+           </div>
+            
+            </a>
+
+          
+       
+          </div>
+          
+
+
+
+          <div class="border mt-4 ">
+          <a href="#">
+          <div class=" text-black">
+          <h4 class="float-start">Hcl</h4>
+          <br/>
+          <br/>
+            <h3 class="float-start">Fronted Developer</h3>
+            <br/> <br/>
+          
+            <h4 class="float-start">Noida</h4>
+            <br/>
+          <br/>
+            <h5 class="float-start">₹4-5 lpa</h5>
+
+ 
+  
+           </div>
+            
+            </a>
+          
+          </div>
+          <div class="border mt-4">
+          <a href="#">
+          <div class=" text-black">
+          <h4 class="float-start">Hcl</h4>
+          <br/>
+          <br/>
+            <h3 class="float-start">Fronted Developer</h3>
+            <br/> <br/>
+          
+            <h4 class="float-start">Noida</h4>
+            <br/>
+          <br/>
+            <h5 class="float-start">₹4-5 lpa</h5>
+
+ 
+  
+           </div>
+            
+            </a>
+          </div>
+          <div class="border mt-4">
+          <a href="#">
+          <div class=" text-black">
+          <h4 class="float-start">Hcl</h4>
+          <br/>
+          <br/>
+            <h3 class="float-start">Fronted Developer</h3>
+            <br/> <br/>
+          
+            <h4 class="float-start">Noida</h4>
+            <br/>
+          <br/>
+            <h5 class="float-start">₹4-5 lpa</h5>
+
+ 
+  
+           </div>
+            
+            </a>
+          
+          </div>
+       
+       </div>
+
+       <div class="mt-5 border w-75 ">
+
+
+          <div class=" d-flex justify-content-column  ">
+            <div class="border ">
+            <h4>Boston Consulting Group</h4> 
+            
+         
+
+
+            <h5>Frontend Developer</h5>
+  
+           <p>New Delhi</p>
+   
+            <p>₹10L - ₹10L (Glassdoor Est.)</p>
+        <div>
+        <p>Who We Are
+
+Boston Consulting Group partners with leaders in business
+ and society to tackle their most important challenges and capture their greatest opportunities. BCG was the pioneer
+  in business strategy when it was founded in 1963. Today, we work closely with clients to embrace a transformational
+   approach aimed at benefiting all stakeholders—empowering organizations to grow, build sustainable competitive
+    advantage, and drive positive societal impact.
+
+</p>
+
+
+        </div>
+        
+        <button type="button" class="btn btn-primary m-2 ">Apply Now</button>
+       <button type="button" class="btn btn-secondary">Save</button>
+
+            </div>
+
+
+            
+         
+         
+        </div>
+
+        
+
+       
+        <div class="border mt-4 ">
+            <h4>Boston Consulting Group</h4> 
+            
+         
+
+
+            <h5>Frontend Developer</h5>
+  
+           <p>New Delhi</p>
+   
+            <p>₹10L - ₹10L (Glassdoor Est.)</p>
+        <div>
+        <p>Who We Are
+
+Boston Consulting Group partners with leaders in business
+ and society to tackle their most important challenges and capture their greatest opportunities. BCG was the pioneer
+  in business strategy when it was founded in 1963. Today, we work closely with clients to embrace a transformational
+   approach aimed at benefiting all stakeholders—empowering organizations to grow, build sustainable competitive
+    advantage, and drive positive societal impact.
+
+</p>
+
+
+        </div>
+        
+        <button type="button" class="btn btn-primary m-2 ">Apply Now</button>
+       <button type="button" class="btn btn-secondary">Save</button>
+
+            </div>
+      
+       
+            <div class="border mt-4 ">
+            <h4>Boston Consulting Group</h4> 
+            
+         
+
+
+            <h5>Frontend Developer</h5>
+  
+           <p>New Delhi</p>
+   
+            <p>₹10L - ₹10L (Glassdoor Est.)</p>
+        <div>
+        <p>Who We Are
+
+Boston Consulting Group partners with leaders in business
+ and society to tackle their most important challenges and capture their greatest opportunities. BCG was the pioneer
+  in business strategy when it was founded in 1963. Today, we work closely with clients to embrace a transformational
+   approach aimed at benefiting all stakeholders—empowering organizations to grow, build sustainable competitive
+    advantage, and drive positive societal impact.
+
+</p>
+
+
+        </div>
+        
+        <button type="button" class="btn btn-primary m-2 ">Apply Now</button>
+       <button type="button" class="btn btn-secondary">Save</button>
+
+            </div>
+      
+       
+       
+       
+       
+       </div>
+
+       
+
+
+
+
+       </div>
+
+     
+
+
+   
+    
     </div>
   );
 }
